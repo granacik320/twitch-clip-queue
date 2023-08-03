a61099e6c024a407e6be96195860eabd76d3300c
Paweł Kuźniak <83468012+granacik320@users.noreply.github.com>
2023-08-03 22:18:45 +0200

    Update AppHeader.tsx

--- src/app/AppHeader.tsx
+++ src/app/AppHeader.tsx
@@ -69,6 +69,7 @@ function AppHeader({ noNav = false }: { noNav?: boolean }) {
               <IfAuthenticated>
                 <NavBarButton to="queue">Queue</NavBarButton>
                 <NavBarButton to="history">History</NavBarButton>
+                <NavBarButton to="toplist">Toplist</NavBarButton>
               </IfAuthenticated>
             </Group>
           )}
